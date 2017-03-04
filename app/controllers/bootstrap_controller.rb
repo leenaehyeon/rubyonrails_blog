0c12d2bec0f1cd949aa4062bb86c147a1ee3629a
leenaehyeon <leenaehyeon>
2017-03-04 11:06:20 +0900

    Prepare to diploy!:)

--- app/controllers/bootstrap_controller.rb
+++ app/controllers/bootstrap_controller.rb
@@ -1,4 +1,22 @@
 class BootstrapController < ApplicationController
-def index
-end
+  def index
+
+    def fizzbuzz(n, f, s)
+      result=[]
+      for i in 1..n
+        if i % f == 0 && i % s == 0
+          result << 'fizzbuzz'
+        elsif i % f == 0
+          result << 'buzz'
+        elsif i % s == 0
+          result << 'fizz'
+        else
+          result << i
+        end
+      end
+      result
+    end
+
+    @data = fizzbuzz(50,3,5)
+  end
 end
